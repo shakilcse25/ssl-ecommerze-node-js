09a28ee2515da56205d7d87bb70f158c62196979
shakilcse25 <shakilcse25@gmail.com>
2022-07-31 01:23:41 +0600

    ssl added

--- index.js
+++ index.js
@@ -1,40 +1,136 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const SSLCommerzPayment = require('sslcommerz-lts')
+const bodyParser = require('body-parser')
+const app = express()
+require('dotenv').config()
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
 app.use(bodyParser.json());
 
-const stripe = require('stripe')('sk_test_51LR3w4CVze8v9k3H3RvO8zPKCZu6CKnyvvAvvqMeRCX02xBr8SBMBPw0Snjr1ZrqnVENCSjU2gVDfXVmiSfEEsjE00eO51pNdU');
+app.get('/', async (req, res) => {
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
+  /** 
+  * Root url response 
+  */
+
+  return res.status(200).json({
+    message: "Welcome to sslcommerz app",
+    url: `${process.env.ROOT}/ssl-request`
+  })
+})
+
+app.get('/ssl-request', async (req, res) => {
+
+  /** 
+  * Create ssl session request 
+  */
+
+  const data = {
+    total_amount: 100,
+    currency: 'BDT',
+    tran_id: 'REF123',
+    success_url: `${process.env.ROOT}/ssl-payment-success`,
+    fail_url: `${process.env.ROOT}/ssl-payment-fail`,
+    cancel_url: `${process.env.ROOT}/ssl-payment-cancel`,
+    shipping_method: 'No',
+    product_name: 'Computer.',
+    product_category: 'Electronic',
+    product_profile: 'general',
+    cus_name: 'Customer Name',
+    cus_email: 'cust@yahoo.com',
+    cus_add1: 'Dhaka',
+    cus_add2: 'Dhaka',
+    cus_city: 'Dhaka',
+    cus_state: 'Dhaka',
+    cus_postcode: '1000',
+    cus_country: 'Bangladesh',
+    cus_phone: '01711111111',
+    cus_fax: '01711111111',
+    multi_card_name: 'mastercard',
+    value_a: 'ref001_A',
+    value_b: 'ref002_B',
+    value_c: 'ref003_C',
+    value_d: 'ref004_D',
+    ipn_url: `${process.env.ROOT}/ssl-payment-notification`,
+  };
+
+  const sslcommerz = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD, false) //true for live default false for sandbox
+  sslcommerz.init(data).then(data => {
+
+    //process the response that got from sslcommerz 
+    //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+
+    if (data?.GatewayPageURL) {
+      return res.status(200).redirect(data?.GatewayPageURL);
+    }
+    else {
+      return res.status(400).json({
+        message: "Session was not successful"
+      });
+    }
+  });
+
 });
- 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
 
-app.get("/test", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
+app.post("/ssl-payment-notification", async (req, res) => {
 
-app.post('/api/doPayment/', (req, res) => {
-  return stripe.charges
-    .create({
-      amount: req.body.amount, // Unit: cents
-      currency: 'usd',
-      source: req.body.tokenId,
-      description: 'Test payment',
-    })
-    .then(result => res.status(200).json(result));
-});
+  /** 
+  * If payment notification
+  */
 
-app.listen(5000);
+  return res.status(200).json(
+    {
+      data: req.body,
+      message: 'Payment notification'
+    }
+  );
+})
 
+app.post("/ssl-payment-success", async (req, res) => {
+
+  /** 
+  * If payment successful 
+  */
+
+  return res.status(200).json(
+    {
+      data: req.body,
+      message: 'Payment success'
+    }
+  );
+})
+
+app.post("/ssl-payment-fail", async (req, res) => {
+
+  /** 
+  * If payment failed 
+  */
+
+  return res.status(200).json(
+    {
+      data: req.body,
+      message: 'Payment failed'
+    }
+  );
+})
+
+app.post("/ssl-payment-cancel", async (req, res) => {
+
+  /** 
+  * If payment cancelled 
+  */
+
+  return res.status(200).json(
+    {
+      data: req.body,
+      message: 'Payment cancelled'
+    }
+  );
+})
+
+app.listen(process.env.PORT, () =>
+  console.log(`ssl app listening on port ${process.env.PORT}!`),
+);
